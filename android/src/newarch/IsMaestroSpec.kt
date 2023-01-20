fa592ab9179b357ebf078f70b39285884205ec04
Jacek Pudysz <jacekpudysz@gmail.com>
2023-01-20 12:44:35 +0100

    chore: make kotlin class open

--- android/src/newarch/IsMaestroSpec.kt
+++ android/src/newarch/IsMaestroSpec.kt
@@ -2,7 +2,7 @@ package com.ismaestro
 
 import com.facebook.react.bridge.ReactApplicationContext
 
-class IsMaestroSpec internal constructor(context: ReactApplicationContext) : NativeIsMaestroSpec(context) {
+open class IsMaestroSpec internal constructor(context: ReactApplicationContext) : NativeIsMaestroSpec(context) {
     private val module: IsMaestroModule by lazy {
         IsMaestroModule(context)
     }
